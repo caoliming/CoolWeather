18c2fd5fe55e64938eb379614d129d219a00e276
15034 <liming.cao@hand-china.com>
2017-11-28 20:18:28 +0800

    cool weather 2017-11-28

---
+++ app/src/main/java/com/cn/coolweather/gson/AQI.java
@@ -0,0 +1,17 @@
+package com.cn.coolweather.gson;
+
+/**
+ * Created by 曹黎明 on 2017/10/9.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
---
+++ app/src/main/java/com/cn/coolweather/gson/Basic.java
@@ -0,0 +1,24 @@
+package com.cn.coolweather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by 曹黎明 on 2017/10/9.
+ */
+
+public class Basic {
+    @SerializedName("city")
+    public String cityName;
+
+    @SerializedName("id")
+    public String weatherId;
+
+    public Update update;
+
+    public class Update{
+        @SerializedName("loc")
+        public String updateTime;
+    }
+
+
+}
